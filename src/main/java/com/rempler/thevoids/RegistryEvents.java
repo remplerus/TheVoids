d88a875bbdb301b0d936121d9800b5de6ea2c9dc
remplerus <44590574+remplerus@users.noreply.github.com>
2020-08-10 17:49:52 +0200

    Put events together

--- src/main/java/com/rempler/thevoids/RegistryEvents.java
+++ src/main/java/com/rempler/thevoids/RegistryEvents.java
@@ -1,7 +1,14 @@
 package com.rempler.thevoids;
 
+import com.rempler.thevoids.block.TheVoidsTileEntity;
+import com.rempler.thevoids.container.TheVoidsContainer;
+import com.rempler.thevoids.init.ModObjects;
 import com.rempler.thevoids.world.TheVoidsBiome;
 import com.rempler.thevoids.world.TheVoidsBiomes;
+import net.minecraft.block.Block;
+import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.event.RegistryEvent;
@@ -20,6 +27,26 @@ public class RegistryEvents {
                 TheVoidsBiomes.the_void = new TheVoidsBiome()
         );
         TheVoidsBiomes.registerBiomes();
+    }
+
+    @SubscribeEvent
+    public static void registerBlock(final RegistryEvent.Register<Block> event) {
+        event.getRegistry().register(ModObjects.STONE_WORKBENCH);
+    }
+
+    @SubscribeEvent
+    public static void registerItem(final RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(ModObjects.getItems());
+    }
+
+    @SubscribeEvent
+    public static void registerTile(final RegistryEvent.Register<TileEntityType<?>> event) {
+        event.getRegistry().register(TheVoidsTileEntity.TYPE);
+    }
+
+    @SubscribeEvent
+    public static void registerContainer(final RegistryEvent.Register<ContainerType<?>> event) {
+        event.getRegistry().register(TheVoidsContainer.TYPE);
     }
 
     public static ResourceLocation location(String name){
