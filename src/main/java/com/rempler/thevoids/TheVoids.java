aa17f5f294fbcb55267666bc9c0267e6d3ba1d31
remplerus <44590574+remplerus@users.noreply.github.com>
2020-08-10 16:18:51 +0200

    wrong updated

--- src/main/java/com/rempler/thevoids/TheVoids.java
+++ src/main/java/com/rempler/thevoids/TheVoids.java
@@ -1,8 +1,11 @@
 package com.rempler.thevoids;
 
+import com.rempler.thevoids.proxy.ClientProxy;
+import com.rempler.thevoids.proxy.CommonProxy;
 import com.rempler.thevoids.world.TheVoidsWorldType;
 import net.minecraft.world.WorldType;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -16,6 +19,7 @@ public class TheVoids
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "thevoids";
     public static final WorldType THE_VOIDS_TYPE = new TheVoidsWorldType();
+    private static final CommonProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> CommonProxy::new);
 
     public TheVoids() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
@@ -24,7 +28,12 @@ public class TheVoids
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    public static CommonProxy getProxy() {
+        return proxy;
+    }
+
     private void setup(final FMLCommonSetupEvent event) {
+        proxy.init();
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
